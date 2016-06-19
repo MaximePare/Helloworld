faedb99641961c1550061ed59d6c50d14d59ceed
Maxime Pare <maxime.pare.4@ulaval.ca>
2016-06-19 16:57:23 -0400

    oups

---
+++ src/main/java/hello/Error.java
@@ -0,0 +1,23 @@
+package hello;
+
+/**
+ * Created by Maxime on 2016-06-19.
+ */
+public class Error {
+
+    private String message;
+
+    public Error(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+
+        return message;
+    }
+
+}
